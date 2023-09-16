b5ccc5a31dc57007ee505b28f5ec8b0e65cbf944
Hamzaswatiio7 <hamzaswatiio7@gmail.com>
2023-09-16 16:50:10 +0500

    Updated climate_q2.py to read data from CSV and plot graphs

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,26 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-years = []
-co2 = []
-temp = []
+
+data = pd.read_csv('C:\\Users\\Msi\\Downloads\\Quiz_week_8-main\\Quiz_week_8-main\\Quiz_week_8\\climate.csv')
+
+
+years = data['Year'].tolist()
+co2 = data['CO2'].tolist()
+temp = data['Temperature'].tolist()
+
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
 
+
+plt.savefig("climate_data_q2.png")
+plt.show()
